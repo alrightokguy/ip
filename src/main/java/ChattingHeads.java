54e6d12a1c52d23b197bf66ba804070c1c628f71
Justen <chongjusten@gmail.com>
2026-08-19 23:34:32 +0800

    Echo

--- src/main/java/ChattingHeads.java
+++ src/main/java/ChattingHeads.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class ChattingHeads {
     public static void main(String[] args) {
-        String banner = "Hello! I'm Chatting Heads.\n" +
-                "You may find yourself\n" +
-                "Living in a shotgun shack\n" +
-                "What can I do for you?\n" +
-                "------------------------------------------------------------\n" +
-                "Letting the days go bye!";
-        System.out.println(banner);
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(
+            "Hello! I'm Chatting Heads.\n" +
+            "You may find yourself\n" +
+            "Living in a shotgun shack\n" +
+            "What can I do for you?"
+        );
+        lineBreak();
+        String input = scan.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            lineBreak();
+            input = scan.nextLine();
+        }
+
+        System.out.println("Letting the days go bye");
+    }
+
+    public static void lineBreak() {
+        System.out.println("------------------------------------------------------------");
     }
 }
