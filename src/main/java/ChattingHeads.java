f3ed8ab4e5c8a705bfebc19ce4fed4a5ea4aa9e2
Justen <chongjusten@gmail.com>
2026-08-19 23:15:28 +0800

    Rename, Greet, Exit

---
+++ src/main/java/ChattingHeads.java
@@ -0,0 +1,11 @@
+public class ChattingHeads {
+    public static void main(String[] args) {
+        String banner = "Hello! I'm Chatting Heads.\n" +
+                "You may find yourself\n" +
+                "Living in a shotgun shack\n" +
+                "What can I do for you?\n" +
+                "------------------------------------------------------------\n" +
+                "Letting the days go bye!";
+        System.out.println(banner);
+    }
+}
