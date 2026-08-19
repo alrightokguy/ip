c34ba6dc39580f4f87a1a26fd404ccc570b07791
Justen <chongjusten@gmail.com>
2026-08-19 23:47:07 +0800

    Add, List

--- src/main/java/ChattingHeads.java
+++ src/main/java/ChattingHeads.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ChattingHeads {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> texts = new ArrayList<>();
 
         System.out.println(
             "Hello! I'm Chatting Heads.\n" +
@@ -14,11 +16,17 @@ public class ChattingHeads {
         String input = scan.nextLine();
 
         while (!input.equals("bye")) {
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < texts.size(); i++) {
+                    System.out.println(String.format("%d. %s", i+1, texts.get(i)));
+                }
+            } else {
+                texts.add(input);
+                System.out.println("added: " + input);
+            }
             lineBreak();
             input = scan.nextLine();
         }
-
         System.out.println("Letting the days go bye");
     }
 
