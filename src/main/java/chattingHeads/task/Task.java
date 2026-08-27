bcc97a3c899783f345d1531ccbeabc0a73ae786c
Justen <chongjusten@gmail.com>
2026-08-28 05:10:32 +0800

    Document Task

--- src/main/java/chattingHeads/task/Task.java
+++ src/main/java/chattingHeads/task/Task.java
@@ -3,25 +3,45 @@ package chattingHeads.task;
 import java.io.IOException;
 import java.time.LocalDateTime;
 
+/**
+ * Represents a task with a description and completion status.
+ */
 public abstract class Task {
 
     private final String description;
     private boolean status;
 
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description Description of the task.
+     */
     public Task(String description) {
         this.description = description;
         status = false;
     }
 
+    /**
+     * Creates a task with the given description and completion status.
+     *
+     * @param description Description of the task.
+     * @param status Completion status of the task.
+     */
     public Task(String description, boolean status) {
         this.description = description;
         this.status = status;
     }
 
+    /**
+     * Marks this task as completed.
+     */
     public void mark() {
         status = true;
     }
 
+    /**
+     * Marks this task as incomplete.
+     */
     public void unmark() {
         status = false;
     }
@@ -31,6 +51,13 @@ public abstract class Task {
         return String.format("[%s] %s", status ? "X" : " ", description);
     }
 
+    /**
+     * Generates a task from its CSV representation.
+     *
+     * @param line CSV representation of the task.
+     * @return Task represented from the CSV data.
+     * @throws IOException If the CSV data is corrupted.
+     */
     public static Task fromCsv(String line) throws IOException {
         String[] fields = line.split(",");
         String type = fields[0];
@@ -51,6 +78,11 @@ public abstract class Task {
         };
     }
 
+    /**
+     * Returns the CSV representation of this task.
+     *
+     * @return CSV representation of this task.
+     */
     public String toCsv() {
         return String.format("%s,%s", description, status);
     }
