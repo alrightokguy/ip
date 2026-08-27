49bd265413209e6f924f02177217fd67f73b2443
Justen <chongjusten@gmail.com>
2026-08-27 23:20:08 +0800

    Laid out Parser skeleton.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,17 +13,46 @@ public class Parser {
             throw new InvalidInputException("command");
         }
         String[] tokens = input.split("\\s+");
+        String command = tokens[0];
+        String[] arguments = Arrays.copyOfRange(tokens, 1, tokens.length);
+
+        switch (command) {
+            case "todo" -> parseTodo(arguments);
+            case "deadline" -> parseDeadline(arguments);
+            case "event" -> parseEvent(arguments);
+            case "list" -> new ListCommand();
+            case "mark" -> new MarkCommand(parseTaskNumber(arguments));
+            case "unmark" -> new UnmarkCommand(parseTaskNumber(arguments));
+            case "delete" -> new DeleteCommand(parseTaskNumber(arguments));
+            case "bye" -> new ExitCommand();
+            default -> throw new InvalidInputException("command");
+        }
+    }
+
+    private void parseTodo(String[] arguments) {
 
     }
 
-    public String parseString(String[] tokens, int start, int end) {
+    private void parseDeadline(String[] arguments) {
+
+    }
+
+    private void parseEvent(String[] arguments) {
+
+    }
+
+    private int parseTaskNumber(String[] arguments) {
+        return Integer.parseInt(arguments[0]);
+    }
+
+    private String parseString(String[] tokens, int start, int end) {
         if (start >= 0 && end <= tokens.length && start <= end) {
             return String.join(" ", Arrays.copyOfRange(tokens, start, end));
         }
         return "";
     }
 
-    public LocalDateTime parseDateTime(String[] tokens, int start, int end) {
+    private LocalDateTime parseDateTime(String[] tokens, int start, int end) {
         try {
             return LocalDateTime.parse(parseString(tokens, start, end), DATE_TIME_FORMATTER);
         } catch (DateTimeParseException e) {
