21364a7919ccd0454bbd1fe6bf184072607f9ce8
Justen <chongjusten@gmail.com>
2026-09-07 20:41:29 +0800

    Incorporated stream usage into TaskList::findIndices

--- src/main/java/chattingheads/storage/Storage.java
+++ src/main/java/chattingheads/storage/Storage.java
@@ -53,7 +53,7 @@ public class Storage {
         try {
             Files.write(filePath, taskList.getTasks().stream().map(Task::toCsv).toList());
         } catch (IOException e) {
-            return;
+            e.printStackTrace();
         }
     }
 }
