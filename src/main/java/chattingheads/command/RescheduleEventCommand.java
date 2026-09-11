54009829087587b8d623081da4158109937b69c4
Justen <chongjusten@gmail.com>
2026-09-12 04:10:11 +0800

    Fix bugs with error handling

--- src/main/java/chattingheads/command/RescheduleEventCommand.java
+++ src/main/java/chattingheads/command/RescheduleEventCommand.java
@@ -2,6 +2,7 @@ package chattingheads.command;
 
 import java.time.LocalDateTime;
 
+import chattingheads.exception.InvalidInputException;
 import chattingheads.exception.InvalidTaskNumberException;
 import chattingheads.exception.InvalidTaskTypeException;
 import chattingheads.task.TaskList;
@@ -31,7 +32,7 @@ public class RescheduleEventCommand extends Command {
 
     @Override
     public String execute(TaskList taskList, Ui ui)
-            throws InvalidTaskNumberException, InvalidTaskTypeException {
+            throws InvalidTaskNumberException, InvalidTaskTypeException, InvalidInputException {
         assert taskList != null : "TaskList should not be null";
         assert ui != null : "Ui should not be null";
 
